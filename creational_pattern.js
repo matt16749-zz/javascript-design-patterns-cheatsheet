110f2fa66432f60b80eaabb97a2fde5feeb0f019
Matthew Chan <matt16749@gmail.com>
2014-10-10 16:42:20 -0700

    removes errorneous note in creational pattern and adds initial singleton pattern

--- creational_pattern.js
+++ creational_pattern.js
@@ -1,6 +1,5 @@
 // Creational Pattern deals with idea of creating new Objects.
 // Forms the basis for many other design patterns.
-// Not as used as much as Module Pattern.(for namespacing.)
 
 // Creation of Object via object literal.
 var creationalObject = {}
---
+++ singleton_pattern.js
@@ -0,0 +1,2 @@
+// Singleton pattern restricts instantiation of a class to a single object.
+// Singletons serve as a way to namespace global variables.
