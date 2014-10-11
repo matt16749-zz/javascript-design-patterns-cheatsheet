7ea1e55f5f774a1eb105593cb38bc3ae2d976ef3
Matthew Chan <matt16749@gmail.com>
2014-10-10 17:58:01 -0700

    adds public and private space to module pattern

--- module_pattern.js
+++ module_pattern.js
@@ -6,18 +6,18 @@
   // Object Literals are a great way to namespace global variables(encapsulate your code)
 
 var Module = function(){
-  // Line 10 is a private variable.
+  // Line 10 is a PRIVATE variable.
   var counter = 0;  
 
   // Object Literal on lines 15-26 is being returned inside the function Module.
   // w/o the object literal you cannot call both incrementCounter and resetCounter, you can only call one (whichever is explicitly returned by Module.)
   return {
-    // incrementCounter is a closure. Also a public function. 
+    // incrementCounter is a closure. Also a PUBLIC function. 
       // Private functions would be functions not being returned inside function Module.
     incrementCounter : function(){
       return counter ++;
     },
-    // resetCounter is a closure. Also a public function.
+    // resetCounter is a closure. Also a PUBLIC function.
     resetCounter : function(){
       console.log("counter value prior to reset: " + counter);
       counter = 0;
