1e0639444e6255438f78034724a422b2e4e2a036
Matthew Chan <matt16749@gmail.com>
2014-10-12 16:07:03 -0700

    initial commit with comments in view.js

---
+++ view.js
@@ -0,0 +1,4 @@
+// Views are a visual representation of the Model. Think of the each View as a seperate page in the DOM.
+// A view will typically observe a model and when the model changes, the view will change.
+// Typically Users interact with views to change attributes of the model.
+// The actual task of updating the model belongs to the controller.
